5b72eab536e5580d257ac6098842c2d75b52d30f
Harshil Gupta <144909381+hars-21@users.noreply.github.com>
2026-02-20 21:21:10 +0530

    Refactor import statements for express types

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,5 @@
-import express, { type Request, type Response } from "express";
+import express from "express";
+import type { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { requestLogger } from "./utils/logger";
